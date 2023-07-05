8460f032e92fceed4f6c503e48fa9b4ddbe656dd
Satyam Rathore <101787698+Satya357@users.noreply.github.com>
2023-07-05 15:28:14 +0530

    Mock_test_02 done

---
+++ JAVA_DSA_PPT/Assignment_09/Mock_test_02/Number_check_01.java
@@ -0,0 +1,13 @@
+public class Number_check_01 {
+    public static void main(String[] args) {
+        int number = 0; // Change this value to check different numbers
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
